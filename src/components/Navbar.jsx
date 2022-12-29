569aa527523053d806157a844cb965d5542077f1
danghung <hungdang02042003@gmail.com>
2022-12-29 14:51:50 +0700

    sidebar ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { logo } from '../utils/constans';
+import SearchBar from './SearchBar';
 
 const Navbar = () => {
-   return <div>Navbar</div>;
+   return (
+      <Stack
+         direction={'row'}
+         alignItems="center"
+         p={2}
+         sx={{
+            position: 'sticky',
+            justifyContent: 'space-between',
+            background: '#000',
+            top: '0',
+            color: '#fff',
+         }}
+      >
+         <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+            <img src={logo} alt="logo" style={{ height: '50px' }} />
+         </Link>
+         <SearchBar />
+      </Stack>
+   );
 };
 
 export default Navbar;
