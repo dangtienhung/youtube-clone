e512b75e56d529a51c989cf30e93031c3bb6fba0
danghung <hungdang02042003@gmail.com>
2023-02-20 12:45:17 +0700

    youtube-clone

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,6 +4,7 @@ import VideoCard from './VideoCard';
 import ChannelCard from './ChannelCard';
 
 const Videos = ({ videos, direction }) => {
+   console.log('🚀 ~ file: Videos.jsx:7 ~ Videos ~ videos', typeof videos);
    if (!videos?.length) {
       return 'Loading...';
    }
